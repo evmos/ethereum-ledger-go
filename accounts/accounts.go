611ad4a19f79cdd1ff37494a138201755cb315f6
Austin Chandra <austinchandra@berkeley.edu>
2022-07-11 16:51:01 -0700

    Copy-paste common types

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -21,8 +21,8 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/evmos/ethereum-ledger-go/common"
 	"golang.org/x/crypto/sha3"
 )
 
---
+++ common/types.go
@@ -0,0 +1,70 @@
+package common
+
+import (
+	"encoding/hex"
+
+	"golang.org/x/crypto/sha3"
+)
+
+// Lengths of hashes and addresses in bytes.
+const (
+	// HashLength is the expected length of the hash
+	HashLength = 32
+	// AddressLength is the expected length of the address
+	AddressLength = 20
+	// 64 bytes ECDSA signature + 1 byte recovery id
+	SignatureLength = 64 + 1
+)
+
+// Address represents the 20 byte address of an Ethereum account.
+type Address [AddressLength]byte
+
+// Hash represents the 32 byte Keccak256 hash of arbitrary data.
+type Hash [HashLength]byte
+
+// Hex returns an EIP55-compliant hex string representation of the address.
+func (a Address) Hex() string {
+	return string(a.checksumHex())
+}
+
+// String implements fmt.Stringer.
+func (a Address) String() string {
+	return a.Hex()
+}
+
+func (a *Address) checksumHex() []byte {
+	buf := a.hex()
+
+	// compute checksum
+	sha := sha3.NewLegacyKeccak256()
+	sha.Write(buf[2:])
+	hash := sha.Sum(nil)
+	for i := 2; i < len(buf); i++ {
+		hashByte := hash[(i-2)/2]
+		if i%2 == 0 {
+			hashByte = hashByte >> 4
+		} else {
+			hashByte &= 0xf
+		}
+		if buf[i] > '9' && hashByte > 7 {
+			buf[i] -= 32
+		}
+	}
+	return buf[:]
+}
+
+func (a Address) hex() []byte {
+	var buf [len(a)*2 + 2]byte
+	copy(buf[:2], "0x")
+	hex.Encode(buf[2:], a[:])
+	return buf[:]
+}
+
+// AccessList is an EIP-2930 access list.
+type AccessList []AccessTuple
+
+// AccessTuple is the element type of an access list.
+type AccessTuple struct {
+	Address     Address `json:"address"        gencodec:"required"`
+	StorageKeys []Hash  `json:"storageKeys"    gencodec:"required"`
+}
